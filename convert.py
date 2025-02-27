65700407e99c96a89cf4e73afa511a6ccaceaf35
Pete Warden <pete@usefulsensors.com>
2025-02-27 15:35:59 -0800

    Added meaningful input and output names

--- convert.py
+++ convert.py
@@ -1,15 +1,85 @@
 from moonshine import load_model
 import os
 import tensorflow as tf
+from tensorflow.python.tools import saved_model_utils
 
-def save_tfl(keras_model, path):
-    converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
+def save_tfl(keras_model, path, input_names=None, output_names=None):
+    saved_model_path = path.replace(".tfl", ".saved_model")
+    # print(keras_model.summary())
+    # print(keras_model.inputs)
+    # exit(1)
+    keras_model.export(saved_model_path, format="tf")
+
+    tag_set = ["serve"]
+    signature_def_key = 'serving_default'
+
+    saved_model = saved_model_utils.read_saved_model(saved_model_path)
+    for meta_graph_def in saved_model.meta_graphs:
+        if meta_graph_def.meta_info_def.tags != tag_set:
+            continue
+        signature_def = meta_graph_def.signature_def[signature_def_key]
+
+        if input_names is not None:
+            assert len(input_names) == len(signature_def.inputs), (
+                f"input_names ({input_names}, len: {len(input_names)}) must have the same length as the number of inputs ({len(signature_def.inputs)})"
+            )
+        for index, (_, info) in enumerate(signature_def.inputs.items()):
+            input_node_name, input_port = info.name.split(":")
+            if input_names is not None:
+                new_node_name = input_names[index]
+            else:
+                new_node_name = f"input_{int(index):03d}"
+            for node in meta_graph_def.graph_def.node:
+                if node.name == input_node_name:
+                    node.name = new_node_name
+                for i, input in enumerate(node.input):
+                    if input == input_node_name:
+                        node.input[i] = f"{new_node_name}:{input_port}"
+                        break                    
+            info.name = f"{new_node_name}:{input_port}"
+
+        if output_names is not None:
+            assert len(output_names) == len(signature_def.outputs), (
+                "output_names must have the same length as the number of outputs"
+            )
+        for index, (_, info) in enumerate(signature_def.outputs.items()):
+            output_node_name, output_node_index = info.name.split(":")
+            if output_names is not None:
+                new_node_name = output_names[index]
+            else:
+                new_node_name = f"output_{int(index):03d}"
+            meta_graph_def.graph_def.node.extend([
+                tf.compat.v1.NodeDef(
+                    name=new_node_name, op="Identity", 
+                    input=[output_node_name + ":" + str(output_node_index)], 
+                    attr={"T": tf.compat.v1.AttrValue(type=info.dtype)},
+                    device="")
+            ])
+            info.name = f"{new_node_name}:0"
+            
+            # for node in meta_graph_def.graph_def.node:
+            #     print(f"Node: {node.name}, {node.op}, {node.input}, {node.attr["T"]}")
+            # exit(0)
+            
+    saved_model_pb_path = os.path.join(saved_model_path, "saved_model.pb")
+
+    # Preserve the original meta graph for debugging purposes.
+    original_model_pb_path = os.path.join(saved_model_path, "saved_model_original.pb")
+    if os.path.exists(original_model_pb_path):
+        os.remove(original_model_pb_path)
+    os.rename(saved_model_pb_path, original_model_pb_path)
+    
+    with open(saved_model_pb_path, "wb") as file:
+        file.write(saved_model.SerializeToString())
+        
+    converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_path)
     tflite_model = converter.convert()
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, 'wb') as f:
         f.write(tflite_model)
 
-for model_name in ["tiny", "base"]:
+# for model_name in ["tiny", "base"]:
+for model_name in ["tiny"]:
     
     model = load_model(f"moonshine/{model_name}")
 
@@ -17,10 +87,44 @@ for model_name in ["tiny", "base"]:
 
     preprocessor_keras = model.preprocessor.preprocess
     encoder_keras = model.encoder.encoder
-    decoder_initial_keras = model.decoder.uncached_call
-    decoder_keras = model.decoder.cached_call
+    decoder_uncached_keras = model.decoder.uncached_call
+    decoder_cached_keras = model.decoder.cached_call
 
+    assert model_name in ["tiny", "base"]
+    if model_name == "tiny":
+        layer_count = 6
+    elif model_name == "base":
+        layer_count = 8
+
+    decoder_uncached_input_names = [
+        "tokens",
+        "audio_features",
+        "seq_len",
+    ]
+
+    decoder_cached_input_names = decoder_uncached_input_names.copy()
+    for index in range(layer_count):
+        decoder_cached_input_names.append(f"input_cache_k_{index}")
+        decoder_cached_input_names.append(f"input_cache_v_{index}")
+        decoder_cached_input_names.append(f"input_x_attn_cache_k_{index}")
+        decoder_cached_input_names.append(f"input_x_attn_cache_v_{index}")
+
+    decoder_output_names = ["logits"]
+    for index in range(layer_count):
+        decoder_output_names.append(f"output_cache_k_{index}")
+        decoder_output_names.append(f"output_cache_v_{index}")
+        decoder_output_names.append(f"output_x_attn_cache_k_{index}")
+        decoder_output_names.append(f"output_x_attn_cache_v_{index}")
+        
     save_tfl(preprocessor_keras, os.path.join(model_name, precision, "preprocessor.tfl"))
     save_tfl(encoder_keras, os.path.join(model_name, precision, "encoder.tfl"))
-    save_tfl(decoder_initial_keras, os.path.join(model_name, precision, "decoder_initial.tfl"))
-    save_tfl(decoder_keras, os.path.join(model_name, precision, "decoder.tfl"))
+    save_tfl(
+        decoder_uncached_keras, 
+        os.path.join(model_name, precision, "decoder_initial.tfl"),
+        input_names=decoder_uncached_input_names, 
+        output_names=decoder_output_names)
+    save_tfl(
+        decoder_cached_keras, 
+        os.path.join(model_name, precision, "decoder.tfl"),
+        input_names=decoder_cached_input_names, 
+        output_names=decoder_output_names)
--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import numpy as np
 import re
-import tensorflow as tf
+#import tensorflow as tf
+import ai_edge_litert.interpreter as tflite
+
 
 def _get_tflite_weights(model_name, precision="float"):
     from huggingface_hub import hf_hub_download
@@ -24,12 +26,33 @@ class MoonshineTFLiteModel(object):
         assert model_name in ["moonshine/tiny", "moonshine/base"], (
             "model_name should be 'moonshine/tiny' or 'moonshine/base'"
         )
-        # These orders were obtained by loading the model in Netron, searching
-        # for "StatefulPartitionedCall_" and observing the results.
-        if model_name == "moonshine/tiny":
-            self.decoder_layer_order = [0, 1, 12, 20, 21, 24, 2, 5, 6, 9, 10, 14, 15]
-        elif model_name == "moonshine/base":
-            self.decoder_layer_order = [0, 1, 12, 28, 29, 32, 2, 5, 6, 9, 10, 14, 15, 18, 19, 22, 24]
+        self.model_name = model_name.split("/")[-1]
+    
+        assert self.model_name in ["tiny", "base"]
+        if self.model_name == "tiny":
+            self.layer_count = 6
+        elif self.model_name == "base":
+            self.layer_count = 8
+
+        self.decoder_uncached_input_names = [
+            "tokens",
+            "audio_features",
+            "seq_len",
+        ]
+
+        self.decoder_cached_input_names = self.decoder_uncached_input_names.copy()
+        for index in range(self.layer_count):
+            self.decoder_cached_input_names.append(f"input_cache_k_{index}")
+            self.decoder_cached_input_names.append(f"input_cache_v_{index}")
+            self.decoder_cached_input_names.append(f"input_x_attn_cache_k_{index}")
+            self.decoder_cached_input_names.append(f"input_x_attn_cache_v_{index}")
+
+        self.decoder_output_names = ["logits"]
+        for index in range(self.layer_count):
+            self.decoder_output_names.append(f"output_cache_k_{index}")
+            self.decoder_output_names.append(f"output_cache_v_{index}")
+            self.decoder_output_names.append(f"output_x_attn_cache_k_{index}")
+            self.decoder_output_names.append(f"output_x_attn_cache_v_{index}")
         
         if model_dir is None:
             decoder_initial, decoder, encoder, preprocessor = self._load_weights_from_hf_hub(
@@ -41,108 +64,93 @@ class MoonshineTFLiteModel(object):
                 for x in ("decoder_initial", "decoder", "encoder", "preprocessor")
             ]
  
-        self.preprocessor = tf.lite.Interpreter(preprocessor)
-        self.encoder = tf.lite.Interpreter(encoder)
-        self.decoder_initial = tf.lite.Interpreter(decoder_initial)
-        self.decoder = tf.lite.Interpreter(decoder)
+
+        self.preprocessor = tflite.Interpreter(
+            preprocessor, 
+            # experimental_op_resolver_type=tflite.experimental.OpResolverType.BUILTIN_WITHOUT_DEFAULT_DELEGATES,
+        )
+        self.encoder = tflite.Interpreter(
+            encoder, 
+            # experimental_op_resolver_type=tflite.experimental.OpResolverType.BUILTIN_WITHOUT_DEFAULT_DELEGATES,
+        )
+        self.decoder_initial = tflite.Interpreter(
+            decoder_initial, 
+            # experimental_op_resolver_type=tflite.experimental.OpResolverType.BUILTIN_WITHOUT_DEFAULT_DELEGATES,
+        )
+        self.decoder = tflite.Interpreter(
+            decoder, 
+            # experimental_op_resolver_type=tf.lite.experimental.OpResolverType.BUILTIN_WITHOUT_DEFAULT_DELEGATES,
+        )
 
         self.preprocessor.allocate_tensors()
         self.encoder.allocate_tensors()
         self.decoder_initial.allocate_tensors()
         self.decoder.allocate_tensors()
 
-    def _sort_by_name(self, names):
-        def atoi(text):
-            return int(text) if text.isdigit() else text
-
-        def natural_keys(text):
-            return [ atoi(c) for c in re.split(r'(\d+)', text) ]
-
-        names.sort(key=natural_keys)
-        return names
-
-    def _sort_inputs(self, input_details):
-        name_dict = {elem['name']: elem['index'] for elem in input_details}
-        return [name_dict[key] for key in self._sort_by_name(list(name_dict.keys()))]
-
-    def _rearrange_indices(self, arr):
-        # Rearrange string sorted (0, 1, 10, 11, 12...) to index sorted (0, 1, 2, 3...).
-        indices_rearrange = [int(j) for j in sorted([str(i) for i in range(len(arr))])]
-        indices_rearrange = [indices_rearrange.index(i) for i in range(len(arr))]
-
-        return [arr[i] for i in indices_rearrange]
-
-    def _sort_outputs(self, output_details):
-        name_dict = {elem['name']: elem['index'] for elem in output_details}
-
-        names_out = [name for name in name_dict.keys() if 'StatefulPartitionedCall_1' in name]
-        indices = [name_dict[key] for key in self._sort_by_name(names_out)]
-
-        return self._rearrange_indices(indices)
-
-
-    def _sort_outputs_decoder(self, output_details):
-        name_dict = {elem['name']: elem['index'] for elem in output_details}
-
-        names_x_attn = [name for name in name_dict.keys() if 'serving_default_keras_tensor' in name]
-        indices_x_attn = [name_dict[key] for key in self._sort_by_name(names_x_attn)]
-
-        # Since outputs are interleaved, there is no clean way to auto map them,
-        # hence this horrible manual map.
-        names_self_attn = [f'StatefulPartitionedCall_1:{d}' for d in self.decoder_layer_order]
-        indices_self_attn = [name_dict[key] for key in names_self_attn]
-
-        # Logits should be first output.
-        indices = [indices_self_attn[0]]
-
-        # Interleave outputs since x attn and self attn kv caches are saved for each layer.
-        for i in range(0, 16, 2):
-            indices += indices_self_attn[i+1:i+3]
-            indices += indices_x_attn[i:i+2]
-
-        return indices
-
     def _load_weights_from_hf_hub(self, model_name, model_precision):
         model_name = model_name.split("/")[-1]
         return _get_tflite_weights(model_name, model_precision)
 
-    def _invoke_tflite(self, interpreter, args, cached_decoder=False):
+    def _invoke_tflite(self, interpreter, inputs, input_names=["input_000"], output_names=["output_000"]):
         input_details = interpreter.get_input_details()
+        assert len(input_details) == len(input_names), f"Expected {len(input_names)} inputs, but got {len(input_details)}"
         output_details = interpreter.get_output_details()
+        assert len(output_details) == len(output_names), f"Expected {len(output_names)} outputs, but got {len(output_details)}"
 
-        # super janky name-based sorting for inputs.
-        input_indices = self._sort_inputs(input_details)
-
+        input_indices = [None] * len(inputs)
+        for input_index, input_name in enumerate(input_names):
+            for input_detail in input_details:
+                name_part = input_detail["name"].split(":")[0]
+                if name_part == input_name:
+                    input_indices[input_index] = input_detail["index"]
+                    break
+        assert None not in input_indices, f"Input names {input_names} not found in input details {input_details}"
+        
         # Resize inputs and allocate tensors.
-        for idx, arg in enumerate(args):
-            interpreter.resize_tensor_input(input_indices[idx], arg.shape)
+        for index, input in enumerate(inputs):
+            interpreter.resize_tensor_input(input_indices[index], input.shape)
         interpreter.allocate_tensors()
 
         # Set inputs and invoke.
-        for idx, arg in enumerate(args):
-            interpreter.set_tensor(input_indices[idx], arg)
+        for index, input in enumerate(inputs):
+            interpreter.set_tensor(input_indices[index], input)
         interpreter.invoke()
 
-        # even jankier hybrid name and topological based sorting for outputs.
-        if cached_decoder:
-            output_indices = self._sort_outputs_decoder(output_details)
-        else:
-            output_indices = self._sort_outputs(output_details)
+        output_indices = [None] * len(output_names)
+        for output_index, output_name in enumerate(output_names):
+            for output_detail in output_details:
+                name_part = output_detail["name"].split(":")[0]
+                if name_part == output_name:
+                    output_indices[output_index] = output_detail["index"]
+                    break
+        assert None not in output_indices, f"Output names {output_names} not found in output details {output_details}"
+
         return [interpreter.get_tensor(idx) for idx in output_indices]
 
     def generate(self, audio, max_len=228):
         features = self._invoke_tflite(self.preprocessor, [audio])[0]
-        embeddings = self._invoke_tflite(self.encoder, [features, np.array(features.shape[-2], dtype=np.int32)])[0]
+        embeddings = self._invoke_tflite(
+            self.encoder, 
+            [features, np.array(features.shape[-2], dtype=np.int32)],
+            ["input_000", "input_001"])[0]
         tokens = np.array([[1]], dtype=np.int32)
         output = tokens
         seq_len = np.array([1], dtype=np.int32)
-        logits, *cache = self._invoke_tflite(self.decoder_initial, [tokens, seq_len, embeddings])
+        logits, *cache = self._invoke_tflite(
+            self.decoder_initial, 
+            [tokens, seq_len, embeddings], 
+            self.decoder_uncached_input_names, 
+            self.decoder_output_names)
         for _ in range(max_len):
             tokens = np.argmax(logits, axis=-1).astype(np.int32)
             output = np.concatenate([output, tokens], axis=-1)
             if tokens[0, 0] == 2:
                 break
             seq_len = seq_len + 1
-            logits, *cache = self._invoke_tflite(self.decoder, [tokens, seq_len, embeddings] + cache, cached_decoder=True)
+            logits, *cache = self._invoke_tflite(
+                self.decoder, 
+                [tokens, seq_len, embeddings] + cache, 
+                self.decoder_cached_input_names, 
+                self.decoder_output_names)
 
         return output
